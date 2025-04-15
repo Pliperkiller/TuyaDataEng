d34e1f024bf3d906b36031e00047f71a5a5ebea4
pliperkiller <pip.kro@gmail.com>
2025-04-14 20:15:24 -0500

    Creados buscadores de analizadores

--- src/analizador/Ianalizador.py
+++ src/analizador/Ianalizador.py
@@ -1,17 +1,6 @@
 from abc import ABC, abstractmethod
 
-class Analizador(ABC):
-    """Parser HTML personalizado para encontrar etiquetas img."""
-    
-    def __init__(self):
-        super().__init__()
-        self.img_tags = []
-        self.current_tag = None
-        self.current_attrs = None
-    
-    def capturar_etiquetas(self, tag, attrs):
+class IAnalizador(ABC):
+    @abstractmethod
+    def handle_starttag(self, tag: str, attrs: str)-> None:
         """Captura las etiquetas img y sus atributos."""
-        if tag == "img":
-            attrs_dict = dict(attrs)
-            if "src" in attrs_dict:
-                self.img_tags.append((tag, attrs))
--- src/buscador_imagenes/buscador_remoto.py
+++ src/buscador_imagenes/buscador_remoto.py
@@ -7,12 +7,12 @@ from urllib.request import urlopen
 class RemoteImageFetcher(IBuscadorImagenes):
     """Obtiene imágenes de URLs remotas."""
     
-    def leet_imagen(self, src: str) -> bytes:
+    def leer_imagen(self, src: str) -> bytes:
         with urlopen(src, timeout=10) as response:
             return response.read()
         
 if __name__ == "__main__":
     # Ejemplo de uso
     fetcher = RemoteImageFetcher()
-    image_data = fetcher.leet_imagen("https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg")
+    image_data = fetcher.leer_imagen("https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg")
     print(f"Datos de la imagen: {image_data[:10]}...")  # Muestra los primeros 10 bytes
